a3e6106392903e165b46f34723d0065b3811e680
TonyMoron20 <tonymoron20@gmail.com>
2022-12-30 13:14:58 -0600

    Se añaden archivos

---
+++ Paddle.cpp
@@ -0,0 +1,60 @@
+#include "Paddle.h"
+#include "agk.h"
+
+const float Paddle::moveSpeed = 360.0f;
+
+Paddle::Paddle(unsigned int image, float startX)
+{
+	//Se inicializa el sprite de la paleta
+	sprite = agk::CreateSprite(image);
+
+	//Se inicializan las coordenadas X e Y que tendra la paleta
+	x = startX;
+	y = agk::GetVirtualHeight()/2.0f;
+
+	//Posición inicial que tendra la paleta
+	agk::SetSpritePositionByOffset(sprite, x, y);
+}
+
+void Paddle::moveUp()
+{
+	move(-1.0f);
+}
+
+void Paddle::moveDown()
+{
+	move(1.0f);
+}
+
+void Paddle::move(float moveDirection)
+{
+	const float moveY = agk::GetFrameTime() * moveSpeed * moveDirection;
+	const float newY = y + moveY;
+
+	//Variables para restringir movimiento vertical de la paleta
+	const float minY = agk::GetSpriteHeight(sprite) / 2.0f;
+	const float maxY = agk::GetVirtualHeight() - minY;
+
+	//Condición para la restricción de movimiento de las paletas
+	if (newY < minY)
+	{
+		y = minY;
+	}
+	else if (newY > maxY)
+	{
+		y = maxY;
+	}
+	else
+	{
+		y = newY;
+	}
+
+	//Actualizar posición de la paleta
+	agk::SetSpritePositionByOffset(sprite, x, y);
+}
+
+void Paddle::reset()
+{
+	y = agk::GetVirtualHeight() / 2.0f;
+	agk::SetSpritePositionByOffset(sprite, x, y);
+}
---
+++ Paddle.h
@@ -0,0 +1,26 @@
+#pragma once
+class Paddle
+{
+	private:
+		//Variable para indicar la velocidad con la que se movera la paleta
+		const static float moveSpeed;
+
+		//Variable para guardar la imagen del sprite
+		unsigned int sprite;
+
+		//Corrdenadas para posicionar a las paletas dentro de la ventana de juego
+		float x, y;
+
+		//Funcion para el movimiento de las paletas
+		void move(float moveDirection);
+
+	public:
+		//Constructor de la clase
+		Paddle(unsigned int image, float startX);
+
+		//Funciones para los distintos comportamientos que tendra la paleta
+		void moveUp();
+		void moveDown();
+		void reset();
+};
+
